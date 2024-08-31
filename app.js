4c2b01d610b1efc48bad9e51f76cf8789a0e269e
rahulshriyash <rahulshinde.backup@gmail.com>
2024-08-31 14:16:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {id: "heading", class:"heading-h1"}, "Welcome Rahul In React World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
